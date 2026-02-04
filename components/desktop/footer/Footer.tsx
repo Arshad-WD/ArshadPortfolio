17c0723f67b7667f0315582ce5cc477fe92e0991
ARSHAD CHAUDHARY <arshadchaudhary9819@gmail.com>
2026-02-04 14:29:29 +0530

    mailto Content change

--- components/desktop/footer/Footer.tsx
+++ components/desktop/footer/Footer.tsx
@@ -47,7 +47,7 @@ export default function Footer(_: FooterProps) {
                 </h2>
                 <div className="mt-16 flex items-center gap-8">
                     <motion.a 
-                        href="mailto:contact@arshad.dev"
+                        href="mailto:jenixweblancer@gmail.com"
                         whileHover={{ scale: 1.05 }}
                         whileTap={{ scale: 0.95 }}
                         className="px-12 py-6 bg-white text-black rounded-full font-black uppercase tracking-widest text-sm shadow-2xl hover:bg-zinc-200 transition-all"
